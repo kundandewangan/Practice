ed11903ac6598f7bf4ca9acd836017a903b936f7
Kundan_Dewangan <kd8donati@gmail.com>
2019-03-20 20:09:49 +0530

    Dynamic Programming

---
+++ Dynamic Programming/Fibonacci.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void fibonacci(int n)
+{
+	// creating a memoized array to store the values. taking n+1 to handle n=0 
+	int i,memo[n+1];
+	memo[0]=0,memo[1]=1; 
+	for(i=2; i<=n; i++)
+	{
+		memo[i]=memo[i-1]+memo[i-2];
+	}
+	cout<<memo[n];
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the nth term"<<endl;
+	cin>>n;
+	fibonacci(n);
+}
